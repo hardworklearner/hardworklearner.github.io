e82f84bc8946c0e59f85dccc35cdf2b4c6a8494b
hardworklearner <hardworklearner@users.noreply.github.com>
2025-07-14 22:47:49 +0000

    deploy: 4618907b0e62f154120645f2a403346a272359af

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,34 @@
-const swconf = { cacheName: 'chirpy-1752519631',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', '/portfoio/', ],allowHosts: [ 'fonts.googleapis.com', 'fonts.gstatic.com', 'fonts.googleapis.com', 'cdn.jsdelivr.net', 'polyfill.io' ],denyPaths: [ ], purge: false };
+const swconf = {
+  
+    cacheName: 'chirpy-1752533266',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+        '/portfoio/',
+      
+
+      
+      
+    ],allowHosts: [
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'polyfill.io'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
