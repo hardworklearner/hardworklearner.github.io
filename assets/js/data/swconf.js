623090d3e390d4267aeb5c77d3502b6501e798c9
hardworklearner <hardworklearner@users.noreply.github.com>
2025-07-15 19:03:37 +0000

    deploy: 36de125829622412d2e5a92c25f5b21e3e00670b

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752605236',resources: [
+    cacheName: 'chirpy-1752606214',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
